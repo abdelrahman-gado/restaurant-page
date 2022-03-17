e5f00da1a0015af9d1c41561d6bb2476bb7edd4a
abdelrahman <gadoua50@gmail.com>
2022-03-17 08:31:19 +0200

    add some styles to the elements of the home page in style.css file

--- src/home-tab.js
+++ src/home-tab.js
@@ -3,13 +3,18 @@ homeDiv.classList.add("home-div");
 
 const heading = document.createElement("h1");
 heading.textContent = "Salasa Resturant";
+heading.classList.add("item");
 
 const welcomeText = document.createElement("p");
 welcomeText.textContent = `Welcome to Salasa Restaurant,
  Here you will find the best pasta with salasa`;
+ welcomeText.classList.add("item", "elements");
+
 
 const hoursUl = document.createElement("ul");
 hoursUl.textContent = "Hours";
+hoursUl.classList.add("item", "elements");
+
 
 const sundayli = document.createElement("li");
 sundayli.textContent = "Sunday: 8am - 8pm";
@@ -26,6 +31,7 @@ hoursUl.appendChild(saturadayli);
 
 const locationDiv = document.createElement("div");
 locationDiv.textContent = "Location";
+locationDiv.classList.add("item", "elements");
 
 const locationPara = document.createElement("p");
 locationPara.textContent = "12st Ahmed Orabi, Cairo, Egypt";
