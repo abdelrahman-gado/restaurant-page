895067a1f4256565355afbf1c0dbbec2947d3da4
abdelrahman <gadoua50@gmail.com>
2022-03-17 06:36:17 +0200

    add the tab switching logic throght event listeners in the index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import { menuDiv } from "./menu-tab.js";
+import { homeDiv } from "./home-tab.js";
+import { contactDiv } from "./contact-tab.js";
+
+
 function createButton(name, className) {
     const btn = document.createElement("button");
     btn.textContent = name;
@@ -18,9 +23,30 @@ const menuBtn = createButton("Menu", "nav-btn");
 const contactBtn = createButton("Contact", "nav-btn");
 
 
+const tabDiv = document.createElement("div");
+tabDiv.style.width = "100%";
+tabDiv.style.height = "100vh";
+
+homeBtn.addEventListener("click", (e) => {
+    tabDiv.innerHTML = "";
+    tabDiv.appendChild(homeDiv);
+});
+
+menuBtn.addEventListener("click", (e) => {
+    tabDiv.innerHTML = "";
+    tabDiv.appendChild(menuDiv);
+});
+
+contactBtn.addEventListener("click", (e) => {
+    tabDiv.innerHTML = "";
+    tabDiv.appendChild(contactDiv);
+});
+
+
 header.appendChild(homeBtn);
 header.appendChild(menuBtn);
 header.appendChild(contactBtn);
 
 
 content.appendChild(header);
+content.appendChild(tabDiv);
